9e920bf9e24e984e33934a576a4bd43c48a2ce9e
renan_macbookair <renan.escalante.chong@gmail.com>
2016-02-23 13:47:44 -0500

    Added main script that calls a hello script

---
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -0,0 +1,7 @@
+/* These are the comments in java script */
+// This another type of comment
+
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello from the outside!';
+
+alert('hello!');
